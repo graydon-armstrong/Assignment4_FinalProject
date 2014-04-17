41f475779d39572458894ba01b2237e9eb4e78b8
Graydon Armstrong <seeds77222@hotmail.com>
2014-04-17 15:19:17 -0400

    Make Login/Logout Link a Button and floating right

--- php/display_login_logout.php
+++ php/display_login_logout.php
@@ -1,10 +1,10 @@
 <?php 
 	if($loggedin==false) 
 	{
-		echo('<a href="login.php">Login</a>');
+		echo('<a href="login.php" style="float:right;" class="button">Login</a>');
 	}
 	else
 	{
-		echo('<a href="php/logout.php">Logout</a>');
+		echo('<a href="php/logout.php" style="float:right;" class="button">Logout</a>');
 	}
 ?>

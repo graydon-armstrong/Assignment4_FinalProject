f12835dfd190326bd3cc62113e2ed33cbebedefe
Graydon Armstrong <seeds77222@hotmail.com>
2014-04-16 20:42:52 -0400

    Login Redirect Initial Commit

---
+++ php/check_login_redirect.php
@@ -0,0 +1,10 @@
+<?php
+	//start a session
+	session_start();
+
+	//see if the user is logged in to the website and if they are not redirect them to a login screen so they cant access this page
+	if(!isset($_SESSION['user_id'])) {
+		header("Location: .\login.php");
+		//echo 'You must login to see this page. Click <a href="login.php">Login</a> to enter your credentials.';
+	}
+?>

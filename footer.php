c7d6ef0e49257c8d4456639b387e7dc0e0c2fa5b
Graydon Armstrong <seeds77222@hotmail.com>
2014-04-17 15:59:28 -0400

    Took out Uneeded Stay connected section on footer

--- footer.php
+++ footer.php
@@ -6,12 +6,6 @@ File Description: This is the footer that is included on every page. It has the 
 -->
 
 <div class="row" id="footer">
-	<div class="large-12 columns">
-		<h4>Stay Connected</h4>
-		<a href="https://www.facebook.com/graydon.arm" class="webicon facebook">Like us on Facebook</a>
-		<a href="https://twitter.com/Graydon_Arm" class="webicon twitter">Follow us on Twitter</a>
-		<a href="#" class="webicon linkedin">Connect to me on LinkedIn</a>
-	</div>
 	<div class="large-12 columns" id="copyright">
 		<p>Copyright Graydons Web Design 2014</p>
 	</div>

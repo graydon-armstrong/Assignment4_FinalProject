38728a7fa63c5270bf87aba467b2d0a9d812a3d7
Graydon Armstrong <seeds77222@hotmail.com>
2014-04-16 23:07:52 -0400

    Incident Dashboard Initial Commit

---
+++ incident_dashboard.php
@@ -0,0 +1,49 @@
+<!--
+Filename: index.php
+Authors name: Graydon Armstrong
+Website name: Graydon Web Design
+File Description: The home page for the website. Has the tag and mission statements, a more work button, and a slider
+-->
+<?php include('php/check_login_redirect.php'); ?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+	<?php include('head.php'); ?>
+	
+	<body>
+	
+	<div class="row" id= "main_layout">
+		<div class="large-12 columns">
+		
+		<?php include('header.php'); ?>
+		
+		<div class="row">
+			<div class="large-12 columns">
+				<h1>Incident Dashboard</h1>
+				<hr />
+			</div>
+		</div>
+
+		<div class="row">
+			<div id="content">
+				<div class="large-12 columns">
+					<p>Incident Dashboard Page Stuff Temp</p>
+				</div>
+			</div>
+		</div>
+
+		<?php include('footer.php') ?>
+		
+		</div>
+	</div>
+	
+	<script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+	
+
+	</body>
+
+</html>
